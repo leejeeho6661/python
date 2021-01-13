dacd51ab4ec4c877c3f1a88d3a66341a813ef183
unknown <leejeeho6661@naver.com>
2021-01-13 15:09:12 +0900

    re

--- week_module_test.py
+++ week_module_test.py
@@ -10,31 +10,27 @@ class module:
         self.pwd=''
         self.selectMenu =''
         self.main()
-    
     #메인 함수
     def main(self):
-        try:
-            print('#'*5,'Main','#'*5)
-            print('1. 로그인')
-            print('2. 회원가입')
-            print('3. 종료')
-            self.selectMenu = input('번호를 선택하세요 : ')
-        except:
-            self.main()
-        else:
-            if self.selectMenu == '1':
-                self.login()
+        print('#'*5,'Main','#'*5)
+        print('1. 로그인')
+        print('2. 회원가입')
+        print('3. 종료')
+        self.selectMenu = input('번호를 선택하세요 : ')
         
-            elif self.selectMenu == '2':
-                self.singUp()
+        if self.selectMenu == '1':
+            self.login()
+        
+        elif self.selectMenu == '2':
+            self.singUp()
             
-            elif self.selectMenu == '3':
-                cursor.close()
-                conn.close()
-                exit()
-            else:
-                print('옳바른 번호를 입력해주세요.\n')
-                self.main()
+        elif self.selectMenu == '3':
+            cursor.close()
+            conn.close()
+            exit()
+        else:
+            print('옳바른 번호를 입력해주세요.\n')
+            self.main()
     #로그인 함수
     def login(self):
         print('#'*5,'Login','#'*5)
